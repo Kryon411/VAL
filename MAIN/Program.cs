2ed5b58c1a198dde05a212cc99cc09b32e14680d
Kryon411 <kerryryan411@hotmail.com>
2026-01-26 21:00:12 -0330

    Fix ValLog namespace import

--- MAIN/Program.cs
+++ MAIN/Program.cs
@@ -4,6 +4,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Options;
+using VAL.Host;
 using VAL.Host.Options;
 using VAL.Host.Services;
 using VAL.Host.Services.Adapters;
