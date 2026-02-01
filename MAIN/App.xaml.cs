307625a19e9c027b8bddb0bdf1e39ac8676e9063
Kryon411 <kerryryan411@hotmail.com>
2026-02-01 18:53:00 -0330

    Load theme resources at startup

--- MAIN/App.xaml.cs
+++ MAIN/App.xaml.cs
@@ -13,6 +13,9 @@ namespace VAL;
 /// </summary>
 public partial class App : Application
 {
+    private static readonly Uri ThemeDictionaryUri =
+        new("pack://application:,,,/VAL;component/UI/VALWindowTheme.xaml", UriKind.Absolute);
+
     private readonly IServiceProvider _services;
 
     public App(IServiceProvider services)
@@ -26,8 +29,37 @@ public partial class App : Application
     {
         base.OnStartup(e);
 
+        EnsureThemeLoaded();
+
         var mainWindow = _services.GetRequiredService<MainWindow>();
         MainWindow = mainWindow;
         mainWindow.Show();
+    }
+
+    private static void EnsureThemeLoaded()
+    {
+        var resources = Current?.Resources;
+        if (resources is null)
+        {
+            return;
+        }
+
+        foreach (var dictionary in resources.MergedDictionaries)
+        {
+            if (dictionary.Source is null)
+            {
+                continue;
+            }
+
+            if (dictionary.Source == ThemeDictionaryUri)
+            {
+                return;
+            }
+        }
+
+        resources.MergedDictionaries.Add(new ResourceDictionary
+        {
+            Source = ThemeDictionaryUri
+        });
     }
 }
